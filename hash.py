716e68ac8562e124dbb00c91d886018ec14d5551
Littleote <david.candelar@gmail.com>
2023-04-26 15:32:06 +0200

    added gitignore and other unimportant stuff
    popipopipipipo

---
+++ hash.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@author: David Candela Rubio and Guillermo Raya García
+"""
+
+import hashlib
+from typing import Optional, Tuple
+
+def uab_md5(message: str, num_bits: int) -> Optional[int]:
+    try:
+        assert(0 < num_bits <= 128)
+        num_shift =  8 - num_bits % 8
+        num_bytes = num_bits // 8 + 1
+        h = hashlib.md5(str.encode(message)).digest()
+        num = int.from_bytes(h[:num_bytes], byteorder='big')
+        num = num >> num_shift
+        print(h)
+        return num
+    except:
+        return None
+
+def second_preimage(message: str, num_bits : int) -> Optional[Tuple[str, int]]:
+    pass
+
+def collision(num_bits: int) -> Optional[Tuple[str, str, int]]:
+    pass
+
+
+uab_md5("abc", 4)
